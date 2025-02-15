35b7f9a57f5bd9ec6c691e0c0ad8b52ea0f5d408
NishDananjaya <dananjayanishanpro@gmail.com>
2025-02-15 23:23:54 +0530

    updated

--- components/footer.tsx
+++
@@ -1,8 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="py-8 text-center">
-      <p>&copy; {new Date().getFullYear()} Nishan Dananjaya. All rights reserved.</p>
-    </footer>
-  )
-}
-
